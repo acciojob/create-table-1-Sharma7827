31d6154affc291f58bcc4e535ade55604ab445fb
Sharma7827 <sharmas4063@gmail.com>
2024-09-25 23:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
+	let table = document.querySelector("#sampleTable")
+	let tr = table.insertRow(0)
+	let cell1 = tr.insertCell()
+	let cell 2 = tr.insertCell()
+	cell1.innerHTML = "New Cell1"
+	cell2.innerHTML = "New Cell2"
+	
   
   
 }
